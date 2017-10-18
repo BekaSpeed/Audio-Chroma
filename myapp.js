0593beb598f5bdb1f0c18bec4f36de2fffa18305
AJ Rasmusson <rasmussonaxj@gmail.com>
2017-10-18 20:58:45 +0000

    multiple box objects are made now

--- myapp.js
+++ myapp.js
@@ -3,23 +3,22 @@ angular.module('myapp',[])
   var colorbox = {
     'background-color' : 'red',
     'color' : 'green',
-    'height' : '10px',
-    'width' : '5%',
-    'block' : 'inline'
+    'height' : '80px',
+    'width' : '50px',
+    'display' : 'inline-block'
   };
   $scope.boxes = [];
   for(i = 0; i < 10; i++) {
     c1 = Math.floor(Math.random()*255).toString(16);
     c2 = Math.floor(Math.random()*255).toString(16);
     c3 = Math.floor(Math.random()*255).toString(16);
-
-    console.log(c1);
+    console.log(c1+c2+c3);
     $scope.boxes.push({
-       'background-color' : c1,
+       'background-color' : '#'+c1+c2+c3,
        'color' : 'green',
-        'height' : '10px',
+       'height' : '10px',
        'width' : '5%',
-       'block' : 'inline'
+       'display' : 'inline-block'
        })
   }
   $scope.box = colorbox;

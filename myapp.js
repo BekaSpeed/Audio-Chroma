d4e02ae9ad4a8f71fd70e220e402ef5775a33b43
AJ Rasmusson <ajrazander@users.noreply.github.com>
2017-10-19 21:46:50 -0600

    Update myapp.js

--- myapp.js
+++ myapp.js
@@ -1,28 +1,18 @@
-angular.module('myapp',[])
+angular.module('myapp',['ngAnimate'])
 .controller('MainCtrl', ['$scope',function($scope) {
-  var colorbox = {
-    'background-color' : 'red',
-    'color' : 'green',
-    'height' : '80px',
-    'width' : '50px',
-    'display' : 'inline-block'
-  };
   $scope.boxes = [];
-  for(i = 0; i < 200; i++) {
+  for(i = 0; i < 1000; i++) {
     hex_color = '';
     for(j = 0; j < 6; j++) {hex_color += Math.floor(Math.random()*16).toString(16);}
     console.log(hex_color);
     $scope.boxes.push({
-       'background-color' : '#'+hex_color,
-       'color' : 'green',
-       'height' : '10px',
-       'width' : '10px',
-        'float':'left',
-        'display' : 'flex',
-        'justify-content' : 'space-around',
-        'margin' : '2px'
-       })
+      'background-color' : '#'+hex_color,
+      'height' : '40px',
+      'width' : '40px',
+      'float':'left',
+      'display' : 'flex',
+      'justify-content' : 'space-around',
+      'margin' : '2px'
+    })
   }
-  $scope.box = colorbox;
-  $scope.data = 'my data';
 }])
